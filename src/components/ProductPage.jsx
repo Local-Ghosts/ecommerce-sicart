39b5b6b8eba3faa698c46b016e4bd07821640ba4
majjikishore007 <majjikishore031@gmail.com>
2021-08-22 18:11:25 +0530

    product page css issue resolved

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./productPage.css";
-
-
+import "../styles/productPage.css";
+import styled from "styled-components";
 const data = {
   img: "https://images.unsplash.com/photo-1629473728190-c9a984fed794?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1864&q=80",
   title: "Pastel Jacket With Bejewelled Collar",
@@ -15,58 +14,71 @@ function ProductPage() {
   const [counter, setCounter] = useState(1);
 
   return (
-    <div className="d-lg-flex justify-content-center product-page">
-      <div className="images-xsm">
-        <div className="product-image-sm">
-          <img src={data.img} alt="" />
+    <Container>
+      <div className='d-lg-flex justify-content-center product-page'>
+        <div className='images-xsm'>
+          <div className='product-image-sm'>
+            <img src={data.img} alt='' />
+          </div>
+          <div className='product-image-sm'>
+            <img src={data.img} alt='' />
+          </div>
+          <div className='product-image-sm'>
+            <img src={data.img} alt='' />
+          </div>
         </div>
-        <div className="product-image-sm">
-          <img src={data.img} alt="" />
+        <div className='product-image'>
+          <img src={data.img} alt='' />
         </div>
-        <div className="product-image-sm">
-          <img src={data.img} alt="" />
-        </div>
-      </div>
-      <div className="product-image">
-        <img src={data.img} alt="" />
-      </div>
-      <div className="details">
-        <h1>{data.title}</h1>
-        <h4 className="price">{data.price}</h4>
-        <label>Size:</label>
-        <select name="" className="dropdown">
-          <option value="">S</option>
-          <option value="">M</option>
-          <option value="">L</option>
-          <option value="">XL</option>
-          <option value="">XXL</option>
-        </select>
-        <div className="d-inline counter">
-          <button
-            onClick={() => {
-              setCounter(counter - 1);
-            }}
-          >
-            -
-          </button>
-          <span className="counter-txt">{counter}</span>
-          <button
-            onClick={() => {
-              setCounter(counter + 1);
-            }}
-          >
-            +
-          </button>
-        </div>
-        <button className="add-to-cart">ADD TO CART</button>
+        <div className='details'>
+          <div>
+            <h1>{data.title}</h1>
+          </div>
+          <div>
+            <h4 className='price'>{data.price}</h4>
+          </div>
+          <div className='price-buttons'>
+            {" "}
+            <label>Size:</label>
+            <select name='' className='dropdown'>
+              <option value=''>S</option>
+              <option value=''>M</option>
+              <option value=''>L</option>
+              <option value=''>XL</option>
+              <option value=''>XXL</option>
+            </select>
+            <div className='d-inline counter'>
+              <button
+                onClick={() => {
+                  setCounter(counter - 1);
+                }}
+              >
+                -
+              </button>
+              <span className='counter-txt'>{counter}</span>
+              <button
+                onClick={() => {
+                  setCounter(counter + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
+            <button className='add-to-cart'>ADD TO CART</button>
+          </div>
 
-        <div className="description">
-          <h5 style={{ fontSize: "12px" }}>DESCRIPTION</h5>
-          <h6>{data.description}</h6>
+          <div className='description'>
+            <h2>DESCRIPTION</h2>
+            <p>{data.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
 export default ProductPage;
+const Container = styled.div`
+  width: 90%;
+  margin-inline: auto;
+`;
