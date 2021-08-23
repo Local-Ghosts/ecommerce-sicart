9834c597ceffb86ed017a744f6c364fa928c1861
majjikishore007 <majjikishore031@gmail.com>
2021-08-23 09:15:31 +0530

    updated

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBnCcKU8jS4EtztyzYnXRorAVcJLhD2fwo",
+  authDomain: "sicart-c7809.firebaseapp.com",
+  projectId: "sicart-c7809",
+  storageBucket: "sicart-c7809.appspot.com",
+  messagingSenderId: "646539583642",
+  appId: "1:646539583642:web:b40a645b6b04a2a0543f23",
+  measurementId: "G-W07CFLRQ5J",
+};
